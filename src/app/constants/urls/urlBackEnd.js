3f2901d9ba7de86251006ba9aa68bb41a6887d26
Yonahey <cherfi.ismael@gmail.comm>
2023-01-09 16:13:48 +0100

    URL_BACK_REGISTRE changes

--- src/app/constants/urls/urlBackEnd.js
+++ src/app/constants/urls/urlBackEnd.js
@@ -1,5 +1,2 @@
 export const URL_BACK_AUTHENTICATE = "/authenticate";
 export const URL_BACK_REGISTER = "/create-buddy";
-
-export const URL_BACK_AUTHENTICATE = "/authenticate";
-export const URL_BACK_REGISTER = "/create-buddy";
