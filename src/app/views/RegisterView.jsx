4ddefa1eeca9adbe018366af2b93e3c712a6afcd
BAEK <t.becq.contact@gmail.com>
2023-01-11 17:12:05 +0100

    style register + trucs

--- src/app/views/RegisterView.jsx
+++ src/app/views/RegisterView.jsx
@@ -153,165 +153,99 @@ const RegisterView = () => {
 				<span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
 			</div>
 
-			<ul className="flex justify-between w-full">
-				<li
-					onClick={setForm}
-					className={
-						page === 0 ? "bg-blue-300 w-2/6 rounded-lg  " : "bg: transparent"
-					}
-				>
-					<div className="flex items-center ">
-						<span className="stepper-head-icon">
-							<svg
-								className="h-8 w-8 text-red-500"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								strokeWidth="2"
-								stroke="currentColor"
-								fill="none"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							>
-								{" "}
-								<path
-									stroke="none"
-									d="M0 0h24v24H0z"
-								/>{" "}
-								<circle
-									cx="12"
-									cy="7"
-									r="4"
-								/>{" "}
-								<path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-							</svg>
-						</span>
-						<span
-							className={
-								page === 0
-									? "ml-2 text-white font-medium"
-									: "ml-2 text-blue-300 cursor-pointer"
-							}
-						>
-							Étape 1/3
-						</span>
-					</div>
-				</li>
-				<li
-					onClick={setForm}
-					className={
-						page === 1 ? "bg-blue-300  w-2/6 rounded-lg" : "bg: transparent "
-					}
-				>
-					<div className="flex items-center">
-						<span className="stepper-head-icon">
-							<svg
-								className="h-8 w-8 text-red-500"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								strokeWidth="2"
-								stroke="currentColor"
-								fill="none"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							>
-								{" "}
-								<path
-									stroke="none"
-									d="M0 0h24v24H0z"
-								/>{" "}
-								<circle
-									cx="12"
-									cy="7"
-									r="4"
-								/>{" "}
-								<path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-							</svg>
-						</span>
-						<span
-							className={
-								page === 1
-									? "ml-2 text-white font-medium"
-									: "ml-2 text-blue-300 cursor-pointer"
-							}
-						>
-							Étape 2/3
-						</span>
-					</div>
-				</li>
-				<li
-					onClick={setForm}
-					className={
-						page === 2 ? "bg-blue-300 w-2/6 rounded-lg" : "bg: transparent"
-					}
-				>
-					<div className="flex items-center">
-						<span className="stepper-head-icon">
-							<svg
-								className="h-8 w-8 text-red-500"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								strokeWidth="2"
-								stroke="currentColor"
-								fill="none"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							>
-								{" "}
-								<path
-									stroke="none"
-									d="M0 0h24v24H0z"
-								/>{" "}
-								<circle
-									cx="12"
-									cy="7"
-									r="4"
-								/>{" "}
-								<path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-							</svg>
-						</span>
-						<span
-							className={
-								page === 2
-									? "ml-2 text-white font-medium"
-									: "ml-2 text-blue-300 cursor-pointer"
-							}
-						>
-							Étape 3/3
-						</span>
-					</div>
-				</li>
-			</ul>
-			<div className="flex-1">{handleForms()}</div>
-			<div className="grid grid-cols-2 gap-4 place-content-center items-center">
-				<button
-					onClick={handlePrev}
-					className="bg-blue-200  hover:bg-blue-300 rounded-md text-gray-800 font-bold py-2 px-4 disabled:bg-gray-400 "
-					disabled={page === 0}
-				>
-					Prev
-				</button>
-				{page === 2 ? (
-					<button
-						onClick={handleSubmit}
-						className="bg-blue-200 hover:bg-blue-300 rounded-md text-gray-800 font-bold py-2 px-4 "
-					>
-						Submit
-					</button>
-				) : (
-					<button
-						onClick={handleNext}
-						className="bg-blue-200 hover:bg-blue-300 rounded-md text-gray-800 font-bold py-2 px-4 "
-					>
-						Next
-					</button>
-				)}
-				<button onClick={test}>envoie</button>
-			</div>
-		</div>
-	);
+      <ul className="flex justify-between w-full">
+        <li onClick={setForm} className={page === 0 ? "bg-vert w-2/6 rounded-lg  " : "bg: transparent"} >
+          <div className="flex items-center ">
+            <span className="stepper-head-icon">
+
+              <svg className="h-8 w-8 text-rose" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <circle cx="12" cy="7" r="4" />  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+            </span>
+            <span
+              className={
+                page === 0
+                  ? "ml-2 text-white font-medium"
+                  : "ml-2 text-gray-500 cursor-pointer"
+              }
+            >
+              Étape 1/3
+            </span>
+          </div>
+        </li>
+        <li
+          onClick={setForm}
+          className={
+            page === 1 ? "bg-vert  w-2/6 rounded-lg" : "bg: transparent "
+          }
+        >
+          <div className="flex items-center">
+            <span className="stepper-head-icon">
+
+              <svg className="h-8 w-8 text-rose" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <circle cx="12" cy="7" r="4" />  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+            </span>
+            <span
+              className={
+                page === 1
+                  ? "ml-2 text-white font-medium"
+                  : "ml-2 text-gray-500 cursor-pointer"
+              }
+            >
+              Étape 2/3
+            </span>
+          </div>
+        </li>
+        <li
+          onClick={setForm}
+          className={
+            page === 2 ? "bg-vert w-2/6 rounded-lg" : "bg: transparent"
+          }
+        >
+          <div className="flex items-center">
+            <span className="stepper-head-icon">
+
+              <svg className="h-8 w-8 text-rose" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <circle cx="12" cy="7" r="4" />  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+            </span>
+            <span
+              className={
+                page === 2
+                  ? "ml-2 text-white font-medium"
+                  : "ml-2 text-gray-500 cursor-pointer"
+              }
+            >
+
+              Étape 3/3
+            </span>
+          </div>
+        </li>
+      </ul>
+      <div className="flex-1">{handleForms()}</div>
+      <div className="grid grid-cols-2 gap-4 place-content-center items-center">
+        <button
+          onClick={handlePrev}
+          className="bg-rose  hover:bg-roseh rounded-md text-gray-600 font-bold py-2 px-4 disabled:bg-gray-400 disabled:bg-opacity-25 disabled:text-opacity-25 "
+          disabled={page === 0}
+        >
+          Prev
+        </button>
+        {page === 2 ? (
+          <button
+            onClick={handleSubmit}
+            className="bg-vert hover:bg-verth rounded-md text-black font-bold py-2 px-4 "
+          >
+            Submit
+          </button>
+        ) : (
+          <button
+            onClick={handleNext}
+            className="bg-vert hover:bg-verth rounded-md text-black font-bold py-2 px-4 "
+          >
+            Next
+          </button>
+
+)}
+{/* <button onClick={test}>envoie</button> */}
+      </div>
+    </div>
+  );
 };
 
 export default RegisterView;
