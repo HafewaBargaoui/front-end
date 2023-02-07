5f22e81b1dfb416b0b6fdcaa5cb4c428311231dd
Hafawa <hafewa.bargaoui@gmail.com>
2023-02-07 20:30:14 +0100

    Modif couleur front

--- src/app/components/account/ResetPassword.jsx
+++ src/app/components/account/ResetPassword.jsx
@@ -66,7 +66,7 @@ const ResetPassword = () => {
 	};
 
   return (
-    <div className="w-full max-w-md space-y-3 rounded-lg  mt-8 pb-8  px-4 shadow sm:px-6 lg:px-8  bg-cover bg-[url('/src/app/assets/images/GradientLogin.png')]">
+    <div className="w-full max-w-md space-y-3 rounded-lg  mt-8 pb-8  px-4 shadow sm:px-6 lg:px-8  bg-cover bg-slate-500">
       <div className="flex justify-center pb-16">
         <h1 className="mt-6 text-center text-3xl font-semibold text-black">
           Réinitialisation de votre mot de passe
@@ -122,7 +122,7 @@ const ResetPassword = () => {
           <div className="text-center">
             <button
               type="submit"
-              className="btn bg-vert group hover:bg-verth relative w-1/2 text-white"
+              className="btn bg-cyan-700 hover:bg-cyan-800 relative w-1/2 text-white font-bold"
             >
               Enregistrer
             </button>
