667a467751ab42f32714d6f41235addffd6f39d0
BAEK <t.becq.contact@gmail.com>
2023-02-20 15:00:04 +0100

    ajout custom input recherche trajet map

--- src/app/components/carte/Carte.jsx
+++ src/app/components/carte/Carte.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../../../../node_modules/leaflet/dist/leaflet.css";
 import "../../app.scss";
-
 import L from "leaflet";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.js";
@@ -11,6 +10,9 @@ import "leaflet-control-geocoder/dist/Control.Geocoder.css"
 const Carte = () => {
   const position = [50.64055652613224, 3.0438356144452072];
   const position1 = [50.54055552613224, 3.14377356144452072];
+  const [routingControl, setRoutingControl] = useState(null);
+  const [depart, setDepart] = useState("Lille, France");
+  const [arrive, setArrive] = useState("Paris, France");
 
   useEffect(() => {
     const map = L.map("map");
@@ -18,7 +20,7 @@ const Carte = () => {
       "https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}@2x.jpg?key=KqtlJiAPF3SvpfVBeuZ4"
     ).addTo(map);
 
-    L.Routing.control({
+    const routingControl = L.Routing.control({
       waypoints: [L.latLng(position), L.latLng(position1)],
       lineOptions: {
         styles: [{
@@ -27,11 +29,42 @@ const Carte = () => {
           opacity: 0.8
         }],
       },
-      geocoder: L.Control.Geocoder.nominatim(),
     }).addTo(map);
-  }, [position, position1]);
+    setRoutingControl(routingControl);
+  }, []);
 
-  return <div id="map" className="rounded-lg z-10 h-5/6 w-5/6"></div>;
+  const submit = (event) => {
+    event.preventDefault();
+    const waypoints = [
+      L.latLng(0, 0),
+      L.latLng(0, 0),
+    ];
+
+    L.Control.Geocoder.nominatim().geocode(depart, function (results) {
+      console.log(results);
+      waypoints[0] = L.latLng(results[0].center.lat, results[0].center.lng);
+      L.Control.Geocoder.nominatim().geocode(arrive, function (results) {
+        waypoints[1] = L.latLng(results[0].center.lat, results[0].center.lng);
+        routingControl.setWaypoints(waypoints);
+      });
+    });
+  };
+
+  return (
+    <>
+      <div id="map" className="rounded-lg z-10 h-5/6 w-4/6"></div>
+      <div className="ml-6">
+        <form onSubmit={submit} method="post" className="grid grid-flow-row ">
+          <input type="text" value={depart} onChange={(e) => setDepart(e.target.value)} />
+          <input type="text" value={arrive} onChange={(e) => setArrive(e.target.value)} />
+          <button type="submit" className="mt-8 bg-vert hover:bg-verth rounded-md text-black  font-normal shadow-md  py-2 px-4">
+            Rechercher
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
+
 
 export default Carte;
