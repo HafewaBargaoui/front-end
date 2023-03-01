e81aa1ec109fda05ed8c8ed7b9bf6621ac58c179
Thomas <Amara.Thomas@outlook.com>
2023-03-01 16:31:56 +0100

    ajou du front pour le systeme de chat

--- src/app/views/chat.jsx
+++ src/app/views/chat.jsx
@@ -64,39 +64,46 @@ const chatSocket = () => {
 
     return (
 
-        <div className="h-full bg-cover bg-[url('./imgs/gradientAccueilCarbuddy.png')]">
-            <h1 className="flex flex-wrap mb-5 justify-center">Messagerie</h1>
+        <div className="h-full bg-cover bg-[#e5f6ec]">
+            <h1 className="flex flex-wrap mb-5 justify-center">Messagerie trajet</h1>
             <section className="flex">
 
-                <div>
-                    <div className="ml-16 flex-wrap w-full h-80 border border-sky-500 bg-white">
+                <div className="mt-16">
+                    <div className="ml-16 flex-wrap w-full h-80 border border-sky-500 rounded-lg bg-[#efefef]">
                         <h5>Rooms</h5>
-                        {roomSelected.map((roomSelected, index) => (
-                            <div key={index}>
-                                <button  onClick={() => joinRoomId(index)}> index N°{index} / {roomSelected.room[0].name}</button>
-                            </div>
-                        ))}
+                        <div className="leading-10">
+
+                            {roomSelected.map((roomSelected, index) => (
+                                <div key={index}>
+                                    <button  onClick={() => joinRoomId(index)}>{roomSelected.room[0].name}</button>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
+                
+                <div className="flex p-5 ml-24 mr-56 mt-16 flex-wrap w-full border border-sky-500 bg-white rounded-lg outline outline-offset-2 outline-blue-500 overflow-y-auto max-h-96">
 
-                <div className="flex ml-24 mr-56 flex-wrap w-full border border-sky-500 bg-white">
-
-                    <div>
+                    <div className="">
                         {history.map((message, index) => (
                             <div key={index}>
-                                <div>{message.user_name} {message.message}</div>
+                                <div>{message.user_name} : {message.message}</div>
                             </div>
                         ))}
 
                         {nameReceived.map((name, index) => (
                             <div key={index}>
-                                <p>{name} {messageReceived[index]}</p>
+                                <p>{name} : {messageReceived[index]}</p>
                             </div>
                         ))}
                     </div>
 
-                    <div className="flex flex-wrap items-end justify-center w-full m-2">
+                </div>
+
+            </section>
+                    <div className="flex flex-wrap justify-center w-full mt-6">
                         <input
+                            className="rounded-lg"
                             type="text"
                             placeholder="message"
                             value={message}
@@ -104,9 +111,6 @@ const chatSocket = () => {
                         />
                         <button onClick={sendMessage}>Envoyé</button>
                     </div>
-
-                </div>
-            </section>
         </div>
     );
 }
