a1929936dbf73861e77c0315f1f734d487457fba
Hafawa <hafewa.bargaoui@gmail.com>
2023-03-07 10:58:59 +0100

    delete email oublié

--- src/app/components/account/ForgetLogin.jsx
+++ src/app/components/account/ForgetLogin.jsx
@@ -1,92 +1,90 @@
-import {Field, Form, Formik, ErrorMessage} from "formik";
-import * as Yup from 'yup'
-import React, {useState} from "react";
-import {useDispatch} from "react-redux";
-import {useNavigate} from "react-router-dom";
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { forgetpassword } from "../../api/backend/account";
-
-
+import { URL_RESET_PASSWORD_MAIL_SENT } from "../../constants/urls/urlFrontEnd";
 const initialValues = {
-	email: '',
-  }
-  
-  const validationSchema = Yup.object({
-	email: Yup.string()
-	  .email('Hum, cette adresse e-mail n’a pas l’air valide. Vérifiez si elle contient bien le "@" et le ".".')
-	  .required('Votre email est obligatoire.'),
-  })
+  email: "",
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email(
+      'Hum, cette adresse e-mail n’a pas l’air valide. Vérifiez si elle contient bien le "@" et le ".".'
+    )
+    .required("Votre email est obligatoire."),
+});
 
 const ForgetLogin = () => {
-	const [errorLog, setErrorLog] = useState(false);
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const [errorLog, setErrorLog] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const handleSubmit = async(values) => {
-		//Send requet to back 
-		//navigate(URL_RESET_PASSWORD);
-		
+  const handleSubmit = async (values) => {
+    //Send requet to back
+    //navigate(URL_RESET_PASSWORD);
 
-		const mail = {
-			email : values.email
-		}
-		console.log(mail);
-		await forgetpassword(mail)
-	};
+    const mail = {
+      email: values.email,
+    };
+    console.log(mail);
+    await forgetpassword(mail);
+    navigate(URL_RESET_PASSWORD_MAIL_SENT);
+  };
 
-	return (
-		<div className="w-full max-w-md space-y-3 rounded-lg  mt-8 pb-8  px-4 shadow sm:px-6 lg:px-8  bg-cover  bg-slate-500">
-			<div className="flex justify-center pb-16">
-				<h1 className="mt-6 text-center text-3xl font-semibold text-black">
-				{/* Saisissez votre adresse e-mail. 
+  return (
+    <div className="w-full max-w-md space-y-3 rounded-lg  mt-8 pb-8  px-4 shadow sm:px-6 lg:px-8  bg-cover  bg-slate-500">
+      <div className="flex justify-center pb-16">
+        <h1 className="mt-6 text-center text-3xl font-semibold text-black">
+          {/* Saisissez votre adresse e-mail. 
 				Nous vous enverrons un lien pour réinitialiser votre mot de passe. */}
-				Mot de passe oublié ?
-				</h1>
+          Mot de passe oublié ?
+        </h1>
 
-				<hr />
-			</div>
+        <hr />
+      </div>
 
-			<Formik
-				 initialValues={initialValues}
-				 validationSchema={validationSchema}
-				 onSubmit={handleSubmit}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form className="mt-8 space-y-6">
+          <div className="flex flex-col space-y-6">
+            <label className="pl-1 font-semibold" htmlFor="email">
+              Email:{" "}
+            </label>
+            <Field
+              type="text"
+              name="email"
+              placeholder="email"
+              autoComplete="email"
+              className="inputInscription"
+            />
+            <ErrorMessage name="email">
+              {(error) => (
+                <div className="text-xs pt-2 text-red-600">{error}</div>
+              )}
+            </ErrorMessage>
+          </div>
 
-				<Form className="mt-8 space-y-6">
-					<div className="flex flex-col space-y-6">
-						<label
-							className="pl-1 font-semibold"
-							htmlFor="email"	
-							>
-							
-							Email:{" "}
-						</label>
-						<Field
-							type="text"
-							name="email"
-							placeholder="email"
-							autoComplete="email"
-							className="inputInscription"
-						/>
-						 <ErrorMessage name='email'>
-                {error => <div className='text-xs pt-2 text-red-600'>{error}</div>} 
-              </ErrorMessage> 
-					</div>
-			
-
-					<div className="text-center">		
-						{/* <div className="flex justify-between pt-8"> */}
-							<button 
-								type="submit"
-								className="btn bg-cyan-700 hover:bg-cyan-800 relative w-1/2 text-white font-bold"
-							>
-								Envoyer
-							</button>
-						{/* </div> */}
-					</div>
-				</Form>
-			</Formik>
-			{/* <p>Retour à la page de connexion</p> */}
-		</div>
-	);
+          <div className="text-center">
+            {/* <div className="flex justify-between pt-8"> */}
+            <button
+              type="submit"
+              className="btn bg-cyan-700 hover:bg-cyan-800 relative w-1/2 text-white font-bold"
+            >
+              Envoyer
+            </button>
+            {/* </div> */}
+          </div>
+        </Form>
+      </Formik>
+      {/* <p>Retour à la page de connexion</p> */}
+    </div>
+  );
 };
 
 export default ForgetLogin;
--- src/app/components/account/Login.jsx
+++ src/app/components/account/Login.jsx
@@ -1,13 +1,17 @@
-import {LockClosedIcon} from "@heroicons/react/solid";
+import { LockClosedIcon } from "@heroicons/react/solid";
 import { EyeOffIcon, EyeIcon } from "@heroicons/react/solid";
-import {Field, Form, Formik} from "formik";
-import React, {useState} from "react";
-import {useDispatch} from "react-redux";
-import {Link, useNavigate} from "react-router-dom";
+import { Field, Form, Formik } from "formik";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
-import {URL_HOME, URL_FORGET_LOGIN, URL_FORGET_EMAIL} from "../../constants/urls/urlFrontEnd";
-import {signIn} from "../../redux-store/authenticationSlice";
-import {authenticate} from "./../../api/backend/account";
+import {
+  URL_HOME,
+  URL_FORGET_LOGIN,
+  URL_FORGET_EMAIL,
+} from "../../constants/urls/urlFrontEnd";
+import { signIn } from "../../redux-store/authenticationSlice";
+import { authenticate } from "./../../api/backend/account";
 
 /**
  * Component Login
@@ -15,104 +19,94 @@ import {authenticate} from "./../../api/backend/account";
  * @author Peter Mollet
  */
 const Login = () => {
-	const [errorLog, setErrorLog] = useState(false);
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const [errorLog, setErrorLog] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-
-	const handleLogin = (values) => {
-		authenticate(values)
-			.then((res) => {
-				if (res.status === 200) {
-					dispatch(signIn(res.data));
-					navigate(URL_HOME);
-				} else {
-					console.log(res.message);
-					dispatch(signIn(res.data));
-					navigate(URL_HOME);
-				} 
-			})
-			.catch((error) => setErrorLog(error));
-	};
+  const handleLogin = (values) => {
+    authenticate(values)
+      .then((res) => {
+        if (res.status === 200) {
+          dispatch(signIn(res.data));
+          navigate(URL_HOME);
+        } else {
+          console.log(res.message);
+          dispatch(signIn(res.data));
+          navigate(URL_HOME);
+        }
+      })
+      .catch((error) => setErrorLog(error));
+  };
   const handleForgetLogin = () => {
+    navigate(URL_FORGET_LOGIN);
+  };
+  const handleForgetEmail = () => {
+    navigate(URL_FORGET_EMAIL);
+  };
 
+  // handle toggle password
 
-					navigate(URL_FORGET_LOGIN);
-				
-			
-	};
-	const handleForgetEmail = () => {
+  const [open, setOpen] = useState(false);
+  const toggle = () => {
+    setOpen(!open);
+  };
+  return (
+    <div className="w-full max-w-xl space-y-3 rounded-lg pb-8 px-4 shadow lg:px-8 bg-cover bg-slate-500">
+      <div className="place-content-center">
+        <h1 className="mt-3 mb-3 text-center text-3xl font-semibold text-black">
+          CONNEXION
+        </h1>
+      </div>
+      <hr />
 
-		navigate(URL_FORGET_EMAIL);
-	
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={handleLogin}
+      >
+        <Form className=" ml-16 mr-16 space-y-6">
+          <div className="flex flex-col space-y-3">
+            <label
+              className="mt-6 text-gray-800 text-md font-semibold"
+              htmlFor="email"
+            >
+              Email :{" "}
+            </label>
+            <Field
+              type="text"
+              name="email"
+              placeholder="michel.dupont@gmail.com"
+              autoComplete="email"
+              className="inputInscription"
+            />
+          </div>
+          <div className="flex flex-col space-y-3 relative">
+            <label
+              className=" text-gray-800 text-md font-semibold"
+              htmlFor="password"
+            >
+              Mot de Passe :{" "}
+            </label>
 
-};
+            <Field
+              type={open === false ? "password" : "text"}
+              name="password"
+              placeholder="***********"
+              autoComplete="current-password"
+              className="inputInscription"
+            />
+            <div className="text-2xl absolute top-9 right-3 z-30 w-4 cursor-pointer text-slate-600">
+              {open === false ? (
+                <EyeOffIcon onClick={toggle} />
+              ) : (
+                <EyeIcon onClick={toggle} />
+              )}
+            </div>
+          </div>
 
-// handle toggle password
-  
-	const [open, setOpen] = useState(false)
-	const toggle = () =>{
-	setOpen(!open)
-};
-	return (
-		<div className="w-full max-w-xl space-y-3 rounded-lg pb-8 px-4 shadow lg:px-8 bg-cover bg-slate-500">
-			<div className="place-content-center">
-				<h1 className="mt-3 mb-3 text-center text-3xl font-semibold text-black">
-					CONNEXION
-				</h1>
-			</div>
-			<hr />
-
-			<Formik
-				initialValues={{
-					email: "",
-					password: "",
-				}}
-				onSubmit={handleLogin}
-			>
-				<Form className=" ml-16 mr-16 space-y-6">
-					<div className="flex flex-col space-y-3">
-						<label
-							className="mt-6 text-gray-800 text-md font-semibold"
-							htmlFor="email"
-						>
-							Email :{" "}
-						</label>
-						<Field
-							type="text"
-							name="email"
-							placeholder="michel.dupont@gmail.com"
-							autoComplete="email"
-							className="inputInscription"
-						/>
-					</div>
-					<div className="flex flex-col space-y-3 relative">
-						<label
-							className=" text-gray-800 text-md font-semibold"
-							
-							htmlFor="password"
-						>
-							Mot de Passe :{" "}
-						</label>
-
-						<Field
-							type={(open === false)? 'password' :'text'}
-							name="password"
-							placeholder="***********"
-							autoComplete="current-password"
-							className="inputInscription"
-						/>
-							<div className="text-2xl absolute top-9 right-3 z-30 w-4 cursor-pointer text-slate-600">
-                  {
-                    
-                      (open === false)? <EyeOffIcon onClick={toggle} />:
-                      <EyeIcon onClick={toggle} />
-                  }   
-                  </div>
-					</div>
-				
-           
-					{/* <div className="mt-3 flex items-center justify-between">
+          {/* <div className="mt-3 flex items-center justify-between">
 						<div className="text-sm">
 							<Link to="/forgot-password">
 								<span className="cursor-pointer font-medium text-primary-dark hover:text-primary">
@@ -121,47 +115,56 @@ const Login = () => {
 							</Link>
 						</div>
 					</div> */}
-
-					<div className="text-center">
-						<button
-							type="submit"
-							className="btn bg-cyan-700 group hover:bg-cyan-800 relative w-1/2 text-white mt-4"
-						>
-							{/* <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+          <p
+            className=" text-slate-700 underline hover:text-cyan-900"
+            onClick={handleForgetLogin}
+          >
+            Mot de passe oublié ?
+          </p>
+          <div className="text-center">
+            <button
+              type="submit"
+              className="btn bg-cyan-700 group hover:bg-cyan-800 relative w-1/2 text-white mt-4"
+            >
+              {/* <span className="absolute inset-y-0 left-0 flex items-center pl-3">
 								<LockClosedIcon
 									className="h-5 w-5 text-primary-dark group-hover:text-primary-light"
 									aria-hidden="true"
 								/>
 							</span> */}
-							Connexion
-						</button>
-						<div className="flex justify-between  mt-8">
-							<button className="btn bg-cyan-600 hover:bg-cyan-500  "
-              					onClick={handleForgetLogin}                        >
-								Mot de passe oublié
-							</button>
+              Connexion
+            </button>
+            <div className="flex justify-between  mt-8">
+              {/* <button
+                className="btn bg-cyan-600 hover:bg-cyan-500  "
+                onClick={handleForgetLogin}
+              >
+                Mot de passe oublié
+              </button> */}
 
-							<button className="btn bg-cyan-600 hover:bg-cyan-500"
-								onClick={handleForgetEmail} 		>
-								Adresse Email oubliée
-							</button>
-						</div>
-					</div>
-					{errorLog && (
-						<div className="flex justify-center">
-							<small className="text-sm italic text-pink-800 font-semibold">
-								Login/Password incorrect(s)
-							</small>
-						</div>
-					)}
+              {/* <button
+                className="btn bg-cyan-600 hover:bg-cyan-500"
+                onClick={handleForgetEmail}
+              >
+                Adresse Email oublié
+              </button> */}
+            </div>
+          </div>
+          {errorLog && (
+            <div className="flex justify-center">
+              <small className="text-sm italic text-pink-800 font-semibold">
+                Login/Password incorrect(s)
+              </small>
+            </div>
+          )}
 
-					<p>
-						{/* vous n'avez pas encore de compte ? <Link to ={}>cliquez ici</Link> */}
-					</p>
-				</Form>
-			</Formik>
-		</div>
-	);
+          <p>
+            {/* vous n'avez pas encore de compte ? <Link to ={}>cliquez ici</Link> */}
+          </p>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default Login;
