6091b272cc4156e8c700bb94ccaff74d1bed758a
Hafawa <hafewa.bargaoui@gmail.com>
2023-03-13 10:13:55 +0100

    Affichage conditionnel nombre de trajets

--- src/app/components/stepsSearchTrajet/SecondStep.jsx
+++ src/app/components/stepsSearchTrajet/SecondStep.jsx
@@ -22,9 +22,15 @@ const SecondStep = ({trajetDepart, trajetArrive, click2, routeSelected, setRoute
   return (
     <div>
       <div className="rounded-xl px-4 shadow-lg lg:px-8 bg-cover bg-center bg-white bg-opacity-30 shadow-gray-900/80">
-        <h6 className="text-center m-1 font-semibold">
-          {routeMatched} Trajets trouvés pour votre recherche :
-        </h6>
+      {routeMatched == 0 ? (
+          <h6> Aucun trajet ne correspond à votre recherche.</h6>
+        ) : routeMatched == 1 ? (
+          <h6> 1 trajet correspond à votre recherche.</h6>
+        ) : (
+          <h6 className="text-center m-1 font-semibold">
+            {routeMatched} Trajets trouvés pour votre recherche :
+          </h6>
+        )}
 
         <div className="mt-6 mx-10">
           {driverRouteSelected.map((route) => (
