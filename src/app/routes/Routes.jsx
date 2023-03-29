f1953b790f1494110b3216816446a6975f582043
BAEK <t.becq.contact@gmail.com>
2023-03-29 09:53:22 +0200

    correction route

--- src/app/routes/Routes.jsx
+++ src/app/routes/Routes.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Route, Routes as RoutesContainer } from "react-router-dom";
 
-import { ROLE_ADMIN } from "../constants/rolesConstant";
 import * as URL from "../constants/urls/urlFrontEnd";
-import AdminHomeView from "../views/AdminHomeView";
-// import HomeView from "../views/HomeView";
+
 import LoginView from "../views/LoginView";
-import AvatarView from "../views/AvatarView";
 import ProfilView from "../views/ProfilView";
 import FaqView from "../views/FaqView";
 import ContactView from "../views/ContactView";
 
 import RegisterView from "../views/RegisterView";
-import { PrivateRoute } from "./PrivateRoute";
 import HomePageView from "../views/HomePageView";
 import ForgetLoginView from "../views/ForgetLoginView";
 import ResetPassword from "../views/ResetPasswordView";
@@ -54,50 +50,6 @@ const Routes = () => {
   return (
     <RoutesContainer>
       <Route path={URL.URL_HOME} element={<HomePageView />} />
-
-      {/* <Route
-        path={URL.URL_CREATE}
-        element={
-          <PrivateRoute>
-            <CreateView />
-          </PrivateRoute>
-        }
-      /> */}
-
-      {/* <Route
-        path={URL.URL_HISTORICAL}
-        element={
-          <PrivateRoute>
-            <HistoricalView />
-          </PrivateRoute>
-        }
-      /> */}
-      {/* <Route exact path="/login" element={<LoginView />} /> */}
-
-      {/* <Route exact path="/create" element={<CreateRoute />} /> 
-
-      <Route exact path="/checkcar" element={<CheckCar />} />  
-
-      <Route exact path="/checkfilters" element={<CheckFilters />} />      
-
-      <Route exact path="/created" element={<Created />} />
-      
-      <Route exact path="/historical" element={<HistoricalView />} />
-
-      <Route exact path="/avatar" element={<AvatarView />} />
-
-      <Route exact path="/gosearch" element={<GoSearch />} />
-
-      <Route exact path="/homePage" element={<HomePageView />} /> */}
-
-      <Route
-        path={URL.URL_ADMIN_HOME}
-        element={
-          <PrivateRoute roles={[ROLE_ADMIN]}>
-            <AdminHomeView />
-          </PrivateRoute>
-        }
-      />
       <Route path={URL.URL_PROFIL} element={<ProfilView />} />
       <Route path={URL.URL_FAQ} element={<FaqView />} />
       <Route path={URL.URL_CONTACT} element={<ContactView />} />
@@ -113,7 +65,6 @@ const Routes = () => {
       <Route path={URL.URL_MAIL_VERIFIED} element={<MailVerified />} />
       <Route path={URL.URL_PROFILEPREFS} element={<ProfilePrefsView />} />
       <Route path={URL.URL_TRAJETS} element={<TrajetsView />} />
-      <Route path={URL.URL_AVATAR} element={<AvatarView />} />
       <Route path={URL.URL_CARTE} element={<CarteView />} />
       <Route path={URL.URL_CHAT_SOCKET} element={<ChatSocket />} />
       <Route path={URL.URL_SEARCH} element={<SearchTrajetView />} />
