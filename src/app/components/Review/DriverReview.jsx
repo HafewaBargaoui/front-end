1e9d8906b46fd726b3945d8e78530612faaa66ba
BAEK <t.becq.contact@gmail.com>
2023-04-04 11:08:32 +0200

    feature front review conducteur

--- src/app/components/Review/DriverReview.jsx
+++ src/app/components/Review/DriverReview.jsx
@@ -7,19 +7,28 @@ const DriverReview = () => {
   const route = queryParameters.get("route");
 
   const [driver, setdriver] = useState([]);
+  const [passenger, setpassenger] = useState([]);
+  const [reviewed, setreviewed] = useState([]);
   const driverProfile = async () => {
     const response = await getDriverReview({ route: route });
-    setdriver(response.data);
+    console.log(response.data);
+    setdriver(response.data.route);
+    setpassenger(response.data.passenger);
+    setreviewed(response.data.reviewed);
   };
 
   const noteDriver = async () => {
-    await driverNote({id : driverId, note: note});
+    await driverNote({id : driverId, note: note, routeId: route,});
   }
 
   useEffect(() => {
     driverProfile();
   }, []);
+  console.log(driver);
+  console.log(passenger);
+  console.log(reviewed);
 
+  
   const name = driver.map((drive) => (drive.id_user.name));
   const driverId = driver.map((drive) => (drive.id_user._id));
   const depart = driver.map((drive) => (drive.starting_location.split(",", [1])));
@@ -30,6 +39,16 @@ const DriverReview = () => {
     <div className='h-screen w-screen absolute top-0 left-0 flex justify-center items-center bg-cover bg-[url("/src/app/assets/images/darkgradient.png")]'>
       <div className="w-full max-w-lg h-96 max-h-lg flex justify-center items-center lg:px-8 bg-white bg-opacity-30 rounded-lg shadow-lg shadow-gray-900/80">
         <div className="grid grid-flow-row">
+        {
+          reviewed ? 
+
+          <p className="text-black text-center font-semibold">
+             vous avez deja noté {name}
+            </p>
+
+          :
+          <>
+
             <p className="text-black text-center font-semibold">
               Vous avez fait le trajet :{depart} / {arrivee}
             </p>
@@ -38,10 +57,12 @@ const DriverReview = () => {
             </p>
             <button 
                   className='bg-vert hover:bg-verth p-1 rounded-md shadow-md uppercase'
-                  onClick={()=>{noteDriver()}}
+                   onClick={()=>{noteDriver()}}
                   >
                   Notez
                 </button>
+          </>
+        }
         </div>
       </div>
     </div>
