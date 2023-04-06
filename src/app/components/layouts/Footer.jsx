8fa27a3b09a8177fb62284a6430e61cddfff15f3
Thomas <Amara.Thomas@outlook.com>
2023-04-06 11:07:24 +0200

    modification du dossier footer

--- src/app/components/layouts/Footer.jsx
+++ src/app/components/layouts/Footer.jsx
@@ -1,57 +1,87 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import { URL_CONTACT, URL_FAQ } from '../../constants/urls/urlFrontEnd';
-import facebook from "../../assets/images/footer/facebook.png"
-import youtube from "../../assets/images/footer/youtube.png"
-import linkedin from "../../assets/images/footer/linkedin.png"
-import twitter from "../../assets/images/footer/twitter.png"
-import instagram from "../../assets/images/footer/instagram.png"
+import { URL_CONTACT, URL_FAQ } from "../../constants/urls/urlFrontEnd";
+import facebook from "../../assets/images/footerHomepage/facebook.png";
+import youtube from "../../assets/images/footerHomepage/youtube.png";
+import linkedin from "../../assets/images/footerHomepage/linkedin.png";
+import twitter from "../../assets/images/footerHomepage/twitter.png";
+import instagram from "../../assets/images/footerHomepage/instagram.png";
 const Footer = () => {
-    const FooterLink = ({to, txt}) => (   
-                    <Link to={to} className="mr-40 text-lg">
-                      {txt}
-                    </Link>             
-      )
-      const FooterHref = ({lien, txt}) => (   
-        <a href ={lien} target="_blank" className="mr-40 text-lg"> {txt}
-        </a>             
-)
-      const FooterImg = ({src, lien, alt, title }) => (
-          <div className="w-14 pr-6">
-              <a href={lien} target="_blank">
-              <img src={src} alt={alt} title={title} />
-              </a>
-          </div>
-      ); 
+  const FooterLink = ({ to, txt }) => (
+    <Link to={to} className="mr-40 text-lg">
+      {txt}
+    </Link>
+  );
+  const FooterHref = ({ lien, txt }) => (
+    <a href={lien} target="_blank" className="mr-40 text-lg">
+      {" "}
+      {txt}
+    </a>
+  );
+  const FooterImg = ({ src, lien, alt, title }) => (
+    <div className="w-14 pr-6">
+      <a href={lien} target="_blank">
+        <img src={src} alt={alt} title={title} />
+      </a>
+    </div>
+  );
   return (
     <div>
-        <div className="flex flex-row bg-black mt-36 px-4 py-12 ">
-          <div className="flex w-56 ml-20">
-            <img src="imgs/CarbuddyWhitepink.png" alt="logo en blanc"></img>
-          </div>
-          <div className="flex flex-row text-white mx-auto">
-            <FooterHref lien="https://mytek-store.fr/cgv" txt="CGV"/>
-            <FooterHref lien="https://mytek-store.fr/mentions-legales" txt="CGU"/>
-            <FooterLink to={URL_FAQ} txt="FAQ"/>
-            <FooterLink to={URL_CONTACT} txt="Nous contacter"/>
-          </div>
-          <div className="flex flex-row w-12 mr-32">
-            <img src="imgs/C_logoWhitecopie.png" alt="logo C en blanc"></img>
-          </div>
+      <div className="flex flex-row bg-black mt-36 px-4 py-12 ">
+        <div className="flex w-56 ml-20">
+          <img src="imgs/CarbuddyWhitepink.png" alt="logo en blanc"></img>
         </div>
-        <div className="flex flex-row bg-black px-4 pb-12">
-          <div className="flex flex-row mx-auto">
-             <FooterImg src={facebook} alt={"facebook"} title ={"facebook"} lien={"https://facebook.com"} />
-             <FooterImg src={twitter} title ={"twitter"} lien={"https://twitter.com"} />
-             <FooterImg src={youtube} title ={"youtube"} lien={"https://youtube.com"} />
-             <FooterImg src={linkedin} title ={"linkedin"} lien={"https://linkedin.com"} />
-             <FooterImg src={instagram} title ={"instagram"} lien={"https://instagram.com"}/>
-          </div>
-          <div className="mx-auto text-white pr-32">
-            <p>Copyright © 2023.Carbuddy Inc.</p>
-          </div>
+        <div className="flex flex-row text-white mx-auto">
+          <FooterHref lien="https://mytek-store.fr/cgv" txt="CGV" />
+          <FooterHref
+            lien="https://mytek-store.fr/mentions-legales"
+            txt="CGU"
+          />
+          <FooterLink to={URL_FAQ} txt="FAQ" />
+          <FooterLink to={URL_CONTACT} txt="Nous contacter" />
+        </div>
+        <div className="flex flex-row w-12 mr-32">
+          <img src="imgs/C_logoWhitecopie.png" alt="logo C en blanc"></img>
         </div>
       </div>
-  )
-}
-export default Footer
+      <div className="flex flex-row bg-black px-4 pb-12">
+        <div className="flex flex-row mx-auto">
+          <FooterImg
+            src={facebook}
+            alt={"facebook"}
+            title={"facebook"}
+            lien={"https://facebook.com"}
+          />
+          <FooterImg
+            src={twitter}
+            alt={"twitter"}
+            title={"twitter"}
+            lien={"https://twitter.com"}
+          />
+          <FooterImg
+            src={youtube}
+            alt={"youtube"}
+            title={"youtube"}
+            lien={"https://youtube.com"}
+          />
+          <FooterImg
+            src={linkedin}
+            alt={"linkedin"}
+            title={"linkedin"}
+            lien={"https://linkedin.com"}
+          />
+          <FooterImg
+            src={instagram}
+            alt={"instagram"}
+            title={"instagram"}
+            lien={"https://instagram.com"}
+          />
+        </div>
+        <div className="mx-auto text-white pr-32">
+          <p>Copyright © 2023.Carbuddy Inc.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Footer;
